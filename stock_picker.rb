fd61f16f025ef75d77efbbc411fc214fa6a415c5
Øistein Haugland <haugland.1193@gmail.com>
2017-02-26 11:37:02 +0100

    finished stock_picker

---
+++ stock_picker.rb
@@ -0,0 +1,37 @@
+
+#du må kjøp før du kan selge
+#Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day. 
+#It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
+
+
+def stock_picker(prices)
+	buy_day = 0
+	buy_value = 0
+
+	sell_day = 0
+	sell_value = 0
+
+	differance = 0
+
+	prices.each_with_index do |a,i|
+		prices.each_with_index do |b,t|
+			if (b - a) > differance && t > i
+
+				buy_day = i
+				buy_value = a 
+
+				sell_day = t
+				sell_value = b
+
+				differance = b - a
+			end
+		end
+	end
+
+	sold = "Bought stock at day #{buy_day} for #{buy_value}$ -  Sold stock at day #{sell_day} for #{sell_value}$. Profit: "
+	profit = sell_value - buy_value
+	return sold + profit.to_s + "$"
+	
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
